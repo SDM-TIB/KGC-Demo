c0b6ab3fe142516f7670d0267030a70de988f782
Ahmad Sakor <sakerobs@gmail.com>
2023-01-31 15:24:28 +0100

    remove unnecessary containers

--- CreationPipeline/scripts/load_to_virtuoso.py
+++ CreationPipeline/scripts/load_to_virtuoso.py
@@ -4,7 +4,6 @@ import os
 
 try:
 	virtuosoIP = os.environ["SPARQL_ENDPOINT_IP"]
-	virtuosoIPMappings = os.environ["SPARQL_MAPPINGS_ENDPOINT_IP"]
 	virtuosoUser = os.environ["SPARQL_ENDPOINT_USER"]
 	virtuosoPass = os.environ["SPARQL_ENDPOINT_PASSWD"]
 	virtuosoPort = os.environ["SPARQL_ENDPOINT_PORT"]
@@ -16,9 +15,6 @@ except Exception as e:
 	exit(-1)
 
 
-os.system("/data/scripts/virtuoso-script.sh " + virtuosoIPMappings + " " + virtuosoUser + " " + virtuosoPass + " " + virtuosoPort + " " + virtuosoGraph + " " + outputfolder_ttl +" ttl")
-for folder in [x[0] for x in os.walk(outputfolder_ttl)][1:]:    
-    os.system("/data/scripts/virtuoso-script.sh " + virtuosoIPMappings + " " + virtuosoUser + " " + virtuosoPass + " " + virtuosoPort + " " + virtuosoGraph + " " + folder +" ttl")
 
 
 os.system("/data/scripts/virtuoso-script.sh " + virtuosoIP + " " + virtuosoUser + " " + virtuosoPass + " " + virtuosoPort + " " + virtuosoGraph + " " + outputfolder_ttl +" ttl")
